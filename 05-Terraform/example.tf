eac1fd909db4db85186593856fc2cd8cb4cf7eb6
Vallard Benincosa <vallard@benincosa.com>
2015-07-06 20:45:46 -0700

    Added Terraform

---
+++ 05-Terraform/example.tf
@@ -0,0 +1,15 @@
+provider "openstack" {
+  user_name = "vallard"  
+}
+
+resource "openstack_compute_instance_v2" "test-server" {
+  name = "tf-test"
+  image_id = "bc3c7ad4-33d5-4702-a01a-b4b65b5c14a3"
+  region = "RegionOne"
+  flavor_id = "5"
+  metadata {
+    this = "that"
+  }
+  key_pair = "tco-gold"
+  security_groups = ["default"]
+}
